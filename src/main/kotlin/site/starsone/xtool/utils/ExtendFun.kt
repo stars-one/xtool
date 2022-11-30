58cd9c91c6d02c945165829cffff9566071681d6
starone <starsone@Foxmail.com>
2022-12-01 00:15:32 +0800

    :sparkles: 新增计算文件大小方法

--- src/main/kotlin/site/starsone/xtool/utils/ExtendFun.kt
+++ src/main/kotlin/site/starsone/xtool/utils/ExtendFun.kt
@@ -3,6 +3,7 @@ package site.starsone.xtool.utils
 import com.google.gson.Gson
 import com.google.gson.internal.`$Gson$Types`
 import com.google.gson.reflect.TypeToken
+import java.text.DecimalFormat
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.reflect.KClass
@@ -14,6 +15,20 @@ fun Long.toDateString(format: String = "yyyy-MM-dd HH:mm:ss"): String {
 
 fun Date.toDateString(format: String = "yyyy-MM-dd HH:mm:ss"): String {
     return SimpleDateFormat(format).format(this)
+}
+
+/**
+ * 将字节(B)转为对应的单位
+ */
+fun Long.toUnitString(): String {
+    val df = DecimalFormat("#.00")
+    val bytes = this
+    return when {
+        bytes < 1024 -> return df.format(bytes.toDouble()) + "B"
+        bytes < 1048576 -> df.format(bytes.toDouble() / 1024) + "KB"
+        bytes < 1073741824 -> df.format(bytes.toDouble() / 1048576) + "MB"
+        else -> df.format(bytes.toDouble() / 1073741824) + "GB"
+    }
 }
 
 /**
