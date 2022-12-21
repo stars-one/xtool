70b4e16d282efd8004134189b791f553252917f6
starone <starsone@Foxmail.com>
2022-12-21 23:55:47 +0800

    :fire: 移除多余文件,整合在MimeMatcher文件里

--- src/main/kotlin/site/starsone/xtool/view/lpk/mime/MimeMatcher.kt
+++ src/main/kotlin/site/starsone/xtool/view/lpk/mime/MimeMatcher.kt
@@ -27,3 +27,36 @@ abstract class MimeMatcher {
         } else false
     }
 }
+
+class MimeMoc : MimeMatcher() {
+    val head = "moc".toByteArray().toList()
+
+    override fun match(data: ByteArray): Boolean {
+        return checkSame(data,head)
+    }
+
+    override fun getMimeType(): String {
+        return "application/moc"
+    }
+
+    override fun getExt(): String {
+        return "moc"
+    }
+}
+
+class MimeMoc3 : MimeMatcher() {
+    var head = "MOC3".toByteArray().toList()
+
+    override fun match(data: ByteArray): Boolean {
+        return checkSame(data,head)
+    }
+
+    override fun getMimeType(): String {
+        return "application/moc3"
+    }
+
+    override fun getExt(): String {
+        return "moc3"
+    }
+
+}
